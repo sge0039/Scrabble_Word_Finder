38c6a7249324a7aaa3102e5c77ca82de979bbde3
sge0039 <48898501+sge0039@users.noreply.github.com>
2023-01-04 22:07:34 +1300

    Update FileLoader.js

--- js/FileLoader.js
+++ js/FileLoader.js
@@ -1,8 +1,6 @@
-class FileLoader {
-	loadFile (file) {
-		fetch(file)
-			.then(response => response.text())
-			// .then(data => console.log(data))
-		  	.then(data => controller.setDictionary(data))
-	}
+function loadFile (file) {
+	fetch(file)
+		.then(response => response.text())
+		// .then(data => console.log(data))
+		.then(data => controller.setDictionary(data))
 }
