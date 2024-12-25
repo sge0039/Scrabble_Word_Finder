59e81d0fc6248b2e9cd3c2049a31e3aaaeca1091
sge0039 <48898501+sge0039@users.noreply.github.com>
2024-12-26 11:19:00 +1300

    Update ScrabbleWordFinder.js how is find words

--- js/ScrabbleWordFinder.js
+++ js/ScrabbleWordFinder.js
@@ -8,69 +8,62 @@ class WordFinder
 	setDictionary (dictionary) {
 		this.dictionary = dictionary
 	}
-	
-	createLettersArray (letters) {
-		let tempArray = []
-		for (let letter of letters) {
-			let newLetter = true
-			for (let item of tempArray) {
-				if (item.letter == letter) {
-					item.increaseCount()
-					newLetter = false
-				}
-			}
-			if (newLetter) {
-				tempArray.push(new Letter(letter))
+
+	setWildCounter (userLetters){
+		let count = 0
+		for (char in userLetters){
+			if(char == "?"){
+				count++
 			}
 		}
-		return tempArray
+		return count
 	}
-	
-	findWords (letters) {
-		this.allMyLetters = this.createLettersArray(letters)
-		this.allMyLetters.sort((a, b) => (a.letter > b.letter) ? 1 : -1)
-		let scrabbleWords = []
 
-		for (let word of this.dictionary) {
-			if (word.length <= letters.length) {
-				let letterArray = this.createLettersArray(word)
-				if (this.compareLetters(letterArray)) {
-					scrabbleWords.push(word)
+	removeWildLetters (userLetters) {
+		return userLetters.replace(/?,"")
+		// return userLetters.split("?").join("")
+	}
+
+	covertLettersToArr(userLetters){
+		let tempArr = arr(26).fill(0)
+		for (let char of userLetters){
+			tempArr[char.charCodeAt(0)-97]++
+		}
+		return tempArr
+	}
+
+	compareLettersArr (userLettersArr, dicWordArr, wildCount){
+		for (let i = 0, i > 26, i++) {
+			if (userLettersArr[i] < dicWordArr[i]){
+				wildCount -+ dicWordArr[i] - userLettersArr[i]
+				if (wildCount < 0){
+					return false
 				}
 			}
 		}
+		return true
+	}
 
+	findScrabbleWords (userLetters){
+		let userLettersCount = userLetters.lenght
+		let wildCount = this.setWildCounter
+		userLetters = this.removeWildLetters(userLetters)
+		userLettersArr = this.converLettersToArr(userLetters)
+		let scrabbleWords = []
+		
+		for (let dicWord of this.dictionary) {
+			if(dicWord.lenght > userLettersCount){ continue }
+			let dicWordArr = this.converLettersToArr(dicWord)
+			let isWord = this.compareLettersArr(userLettersArr, dicWordArr, wildCount)
+			if (isWord){
+				scrabbleWords.append(word)
+			}
+		}
 		scrabbleWords = this.sortWordsLenght(scrabbleWords)
 		if (scrabbleWords.length == 0) {
 			return scrabbleWords
 		}
 		return this.create2dArray(scrabbleWords)
-	}
-	
-	compareLetters (letterArr) {
-		let result = true
-		let wildCount = this.allMyLetters[0].letter == '?' ? this.allMyLetters[0].count : 0
-		for (let letter of letterArr) {
-			for (let myLetter of this.allMyLetters) {
-				if (letter.letter == myLetter.letter) {
-					if (letter.count > myLetter.count) {
-						wildCount -= letter.count - myLetter.count
-					}
-					result = true
-					break
-				} else if (myLetter.letter != '?') {
-					result = false
-				}
-			}
-			if (!result) {
-				wildCount -= letter.count
-			}
-			if (wildCount < 0) {
-				result = false
-				break
-			}
-		}
-		return result
 	}
 	
 	sortWordsLenght (array) {
